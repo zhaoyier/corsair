dc054b1b0b2118c1ea169fb2e4aa4189fecaffca
zhaojianwei <zhaojianwei@lightinthebox.com>
2021-12-04 21:51:54 +0800

    v0.0.31

--- digger/service/fountain/service.go
+++ digger/service/fountain/service.go
@@ -1,11 +1,48 @@
 package fountain
 
 import (
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+
 	"git.ezbuy.me/ezbuy/corsair/digger/common"
 	"github.com/gin-gonic/gin"
 )
 
 func Start() {
+	router := gin.Default()
+	router.Use(common.Cors())
+	startRoute(router)
+
+	server := &http.Server{
+		Addr:    ":12303",
+		Handler: router,
+	}
+
+	quit := make(chan os.Signal)
+	signal.Notify(quit, os.Interrupt)
+
+	go func() {
+		<-quit
+		log.Println("receive interrupt signal")
+		if err := server.Close(); err != nil {
+			log.Fatal("Server Close:", err)
+		}
+	}()
+
+	if err := server.ListenAndServe(); err != nil {
+		if err == http.ErrServerClosed {
+			log.Println("Server closed under request")
+		} else {
+			log.Fatal("Server closed unexpect")
+		}
+	}
+
+	log.Println("Server exiting")
+}
+
+func Start2() {
 	router := gin.Default()
 	router.Use(common.Cors())
 	go startRoute(router)
