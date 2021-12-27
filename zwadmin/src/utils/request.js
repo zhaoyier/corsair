98199a56b90f571633dbc00f8518d0b272ec3a4a
zhaojianwei <zhaojianwei@lightinthebox.com>
2021-12-27 22:42:25 +0800

    v0.0.60

--- zwadmin/src/utils/request.js
+++ zwadmin/src/utils/request.js
@@ -6,7 +6,9 @@ import { getToken } from '@/utils/auth'
 // create an axios instance
 const service = axios.create({
   // baseURL: process.env.VUE_APP_BASE_API, // url = base url + request url
-  baseURL: "http://localhost:12303",
+  // baseURL: "http://localhost:12303",
+  baseURL: "http://api.ibet.sale",
+
   // withCredentials: true, // send cookies when cross-domain requests
   timeout: 5000 // request timeout
 })
