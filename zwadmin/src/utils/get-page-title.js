ed347e7021872dac7aae9b3a679a89aed7c8a4e6
zhaojianwei <zhaojianwei@lightinthebox.com>
2021-12-31 11:54:00 +0800

    v0.0.68

--- zwadmin/src/utils/get-page-title.js
+++ zwadmin/src/utils/get-page-title.js
@@ -1,6 +1,6 @@
 import defaultSettings from '@/settings'
 
-const title = defaultSettings.title || 'Vue Admin Template'
+const title = defaultSettings.title || '磨刀4'
 
 export default function getPageTitle(pageTitle) {
   if (pageTitle) {
